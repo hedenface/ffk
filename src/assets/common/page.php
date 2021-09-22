435c1b70ef96916ca2f550be20e239624b33e587
Bryan Heden <bryan.heden@ticketmaster.com>
2021-09-22 16:52:51 -0400

    get better page layout, actual nav and everything lined up

--- src/assets/common/page.php
+++ src/assets/common/page.php
@@ -12,6 +12,10 @@ function page_header($title, $basedir = ".")
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
+    <!--
+    <script src="<?php echo $basedir; ?>/assets/js/jquery.js"></script>
+    <script src="<?php echo $basedir; ?>/assets/js/bootstrap.bundle.js"></script>
+    -->
     <link rel="stylesheet" href="<?php echo $basedir; ?>/assets/css/bootstrap.css" />
 
     <script src="<?php echo $basedir; ?>/assets/js/drag.js"></script>
@@ -20,6 +24,47 @@ function page_header($title, $basedir = ".")
 </head>
 
 <body>
+
+<div class="wrapper">
+
+<nav class="sidebar">
+    <div class="list-group list-group-flush">
+
+        <div class="sidebar-header border-bottom bg-light">FFK</div>
+
+        <?php if (logged_in(false)) { ?>
+
+        <a class="list-group-item list-group-item-action list-group-item-light new-item" href="#">
+            <strong>New Item</strong>
+            <span><strong>&plus;</strong></span>
+        </a>
+        <a class="list-group-item list-group-item-action list-group-item-light" href="board.php">Board</a>
+        <a class="list-group-item list-group-item-action list-group-item-light" href="settings.php">Settings</a>
+        <a class="list-group-item list-group-item-action list-group-item-light" href="users.php">Users</a>
+        <a class="list-group-item list-group-item-action list-group-item-light new-item" href="new-board.php">New Board<span><strong>&plus;</strong></span></a>
+
+        <?php } else { /* not logged in */ ?>
+
+        <a class="list-group-item list-group-item-action list-group-item-light" href="users.php">Login</a>
+
+        <?php } ?>
+
+    </div>
+</nav>
+
+<div class="main">
+
+<div class="title-bar">
+    <?php echo $title; ?>
+
+    <?php if (logged_in(false)) { ?>
+
+    <div class="logout"><a href="logout.php">Logout</a></div>
+
+    <?php } ?>
+
+</div>
+
 <div class="container">
 
     <?php
@@ -30,7 +75,9 @@ function page_footer()
 {
     ?>
 
-</div>
+</div><!-- .container -->
+</div><!-- .main -->
+</div><!-- .wrapper -->
 </body>
 </html>
 
---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once(dirname(__FILE__) . "/assets/common/includes.php");
+
+ffk_session_start();
+
+page_header("Home");
+
+page_footer();
